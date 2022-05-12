3a4cb49ddb994cd83fd9621483e2bc4f7bc31927
Ruziev-Gofur <gafur_ru@bk.ru>
2022-05-12 14:38:16 +0500

    barber papka yaratdim

--- app/Models/Barber.php
+++ app/Models/Barber.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Barber extends Model
 {
     use HasFactory;
+    protected $table = 'barbers';
+    protected $primaryKey = 'id';
+    protected $fillable = ['border_name', 'barder_phone_number', 'barder_home_adress', 'passport_number', 'work_time'];
 }
